38bc1d4ee64928213d6a0e636f8da1aea5e51896
yeabsirahaile <yeabsirahaile01@gmail.com>
2025-02-03 15:27:17 +0300

    first commi

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
+  host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "phpmyadmin",
   password: process.env.DB_PASSWORD || "yegara",
   database: process.env.DB_NAME || "payment_a",
   port: 3306, // Ensure the port is explicitly defined
 });
-//add this
 
 module.exports = db;
