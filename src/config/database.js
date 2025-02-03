f22f9fd3596d3e5f812f40144efa5ef0d2cff7d1
yeabsirahaile <yeabsirahaile01@gmail.com>
2025-02-03 16:21:15 +0300

    first commi

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1", // Ensure IPv4 is used
-  user: process.env.DB_USER || "phpmyadmin",
-  password: process.env.DB_PASSWORD || "yegara",
-  database: process.env.DB_NAME || "payment_a",
+  host: "127.0.0.1", // Ensure IPv4 is used
+  user: "phpmyadmin",
+  password: "yegara",
+  database: "payment_a",
   port: 3306,
   // Force IPv4 resolution
   multipleStatements: true,
